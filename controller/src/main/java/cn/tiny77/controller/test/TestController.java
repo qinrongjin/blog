aa2ad40150a1222377584b7ea418205081865c09
qinrongjin <qinrongjin@126.com>
2017-09-12 21:12:37 +0800

    java

---
+++ controller/src/main/java/cn/tiny77/controller/test/TestController.java
@@ -0,0 +1,16 @@
+package cn.tiny77.controller.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("test")
+	@ResponseBody
+	public String test() {
+		return "Hello World";
+	}
+	
+}
