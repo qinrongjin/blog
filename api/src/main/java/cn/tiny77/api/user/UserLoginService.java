f39060dc174e98701b34665ec52f96cd3af3bc03
qinrongjin <qinrongjin@126.com>
2017-09-13 22:28:45 +0800

    注册功能

--- api/src/main/java/cn/tiny77/api/user/UserLoginService.java
+++ api/src/main/java/cn/tiny77/api/user/UserLoginService.java
@@ -2,4 +2,6 @@ package cn.tiny77.api.user;
 
 public interface UserLoginService {
 
+	public boolean Register(String username, String email);
+	
 }
---
+++ service/src/main/java/cn/tiny77/service/user/UserLoginServiceImpl.java
@@ -0,0 +1,16 @@
+package cn.tiny77.service.user;
+
+import org.springframework.stereotype.Service;
+
+import cn.tiny77.api.user.UserLoginService;
+
+@Service
+public class UserLoginServiceImpl implements UserLoginService {
+	
+	@Override
+	public boolean Register(String username, String email) {
+		
+		return false;
+	}
+
+}
